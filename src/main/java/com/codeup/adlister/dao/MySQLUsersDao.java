d945d1079640f5b9cd3603d23b9c232d32ad00e4
Ryan Harris <ryan.t.harris67@gmail.com>
2022-11-14 15:46:59 -0600

    update password functions!

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -1,6 +1,7 @@
 package com.codeup.adlister.dao;
 
 import com.codeup.adlister.models.User;
+import com.codeup.adlister.util.Password;
 import com.mysql.cj.jdbc.Driver;
 
 import java.sql.*;
@@ -66,12 +67,13 @@ public class MySQLUsersDao implements Users {
 
     @Override
     public User updateUser(User user) {
-        String query = "UPDATE users SET username = ?, email = ? WHERE id = ?";
+        String query = "UPDATE users SET username = ?, email = ?, password = ? WHERE id = ?";
         try {
             PreparedStatement stmt = connection.prepareStatement(query);
             stmt.setString(1, user.getUsername());
             stmt.setString(2, user.getEmail());
-            stmt.setLong(3, user.getId());
+            stmt.setString(3, user.getPassword());
+            stmt.setLong(4, user.getId());
             stmt.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException("Error creating new user", e);
