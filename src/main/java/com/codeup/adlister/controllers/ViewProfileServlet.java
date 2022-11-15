9f6c9e6ff301205221fe15c954298a50b0115a51
Malik <willis.malik.r@gmail.com>
2022-11-15 14:51:43 -0600

    comparing main

--- src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
+++ src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
@@ -3,6 +3,7 @@ package com.codeup.adlister.controllers;
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.User;
 import com.codeup.adlister.util.Password;
+import com.codeup.adlister.util.PasswordValidation;
 import com.codeup.adlister.util.Validate;
 
 import javax.servlet.ServletException;
@@ -22,7 +23,7 @@ public class ViewProfileServlet extends HttpServlet {
         request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
     }
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-//        long id = Long.parseLong(request.getParameter("id"));
+        long id = Long.parseLong(request.getParameter("id"));
         String username = request.getParameter("username");
         String email = request.getParameter("email");
         String currentPassword = request.getParameter("currentPassword");
@@ -30,25 +31,24 @@ public class ViewProfileServlet extends HttpServlet {
         String passwordConfirmation = request.getParameter("confirm_password");
         String sessionPassword = DaoFactory.getUsersDao().findByUsername(username).getPassword();
         String confirm = request.getParameter("confirm");
-
-//        boolean confirmDelete = confirm.equals("confirm");
+        boolean validAttempt = Password.check(currentPassword, sessionPassword);
+        boolean confirmDelete = confirm.equals("confirm");
+        String delete = request.getParameter("delete");
+        String update = request.getParameter("update");
 
         // valid attempt must be before setting value of currentPassword to a newPassword
-        boolean validAttempt = Password.check(currentPassword, sessionPassword);
+
 
 //Todo - both functionalities work if one (or the other) is commented out. When both in method an error occurs related to confirm being null.
-        Validate validate = new Validate(username, newPassword, confirmPassword);
+        PasswordValidation pv = new PasswordValidation(id, username, email, currentPassword, newPassword, passwordConfirmation, sessionPassword, confirm);
 
-        if (validate.updateUser()) {
-            response.sendRedirect("/profile");
-        } else {
-            response.sendRedirect("/index");
+        if(delete.equals("true")){
+            pv.deleteCredentials(username, currentPassword, sessionPassword, confirm);
         }
-        if (validate.deleteUser(confirm)) {
-            response.sendRedirect("/profile");
-        } else {
-            response.sendRedirect("/index");
+        if(update.equals("true")){
+            pv.changeCredentials(username, email, currentPassword, newPassword, passwordConfirmation, sessionPassword);
         }
+
 
         // TODO: 11/15/22 Update else redirect to appropriate redirect or error message
     }
--- src/main/java/com/codeup/adlister/util/PasswordValidation.java
+++ src/main/java/com/codeup/adlister/util/PasswordValidation.java
@@ -3,9 +3,12 @@ package com.codeup.adlister.util;
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.User;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class PasswordValidation {
+    long id;
     String username;
     String email;
     String currentPassword ;
@@ -15,8 +18,12 @@ public class PasswordValidation {
     String confirm;
 
     HttpServletResponse response;
+    HttpServletRequest request;
 
-    public PasswordValidation(String username, String email, String currentPassword, String newPassword, String passwordConfirmation, String sessionPassword, String confirm) {
+
+
+    public PasswordValidation(long id, String username, String email, String currentPassword, String newPassword, String passwordConfirmation, String sessionPassword, String confirm) {
+        this.id = id;
         this.username = username;
         this.email = email;
         this.currentPassword = currentPassword;
@@ -24,24 +31,34 @@ public class PasswordValidation {
         this.passwordConfirmation = passwordConfirmation;
         this.sessionPassword = sessionPassword;
         this.confirm = confirm;
+
     }
 
+    public void changeCredentials(String username, String email, String currentPassword, String newPassword, String passwordConfirmation, String sessionPassword) throws IOException {
+            boolean validAttempt = Password.check(currentPassword, sessionPassword);
+            if (! newPassword.isEmpty() && newPassword.equals(passwordConfirmation)) currentPassword = newPassword;
 
-//    if (! newPassword.isEmpty() && newPassword.equals(passwordConfirmation)) currentPassword = newPassword;
-//        if (validAttempt) {
-//        User user = new User(username, email, Password.hash(currentPassword));
-//        DaoFactory.getUsersDao().updateUser(user);
-//        response.sendRedirect("/profile");
-//    } else {
-//        response.sendRedirect("/index");
-//    }
-//        if (validAttempt && confirm.equals("confirm")) {
-//        DaoFactory.getUsersDao().deleteUser(username);
-//        request.getSession().removeAttribute("user");
-//        request.getSession().invalidate();
-//        response.sendRedirect("/login");
-//    } else {
-//        response.sendRedirect("/index");
-//    }
+            if (validAttempt) {
+                User user = new User(username, email, Password.hash(currentPassword));
+                DaoFactory.getUsersDao().updateUser(user);
+                response.sendRedirect("/profile");
+            }else {
+                response.sendRedirect("/index");
+            }
+    }
+
+    public void deleteCredentials(String username, String currentPassword, String sessionPassword, String confirm) throws IOException {
+        boolean validAttempt = Password.check(currentPassword, sessionPassword);
+        if (validAttempt && confirm.equals("confirm")) {
+            DaoFactory.getUsersDao().deleteUser(username);
+            request.getSession().removeAttribute("user");
+            request.getSession().invalidate();
+            response.sendRedirect("/login");
+        } else {
+            response.sendRedirect("/index");
+        }
+
+    }
+
 
 }
