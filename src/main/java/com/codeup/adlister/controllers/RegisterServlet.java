d27af0f9feb1385ec782aa9e0bea663431b42026
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-11-17 11:02:15 -0600

    after a user registers.. - set attribute of user and userID for session and redirect to profile page

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -40,6 +40,9 @@ public class RegisterServlet extends HttpServlet {
         // create and save a new user
         User user = new User(username, email, password);
         DaoFactory.getUsersDao().insert(user);
-        response.sendRedirect("/login");
+        request.getSession().setAttribute("user", user);
+        request.getSession().setAttribute("userId", user.getId());
+        response.sendRedirect("/profile");
+
     }
 }
